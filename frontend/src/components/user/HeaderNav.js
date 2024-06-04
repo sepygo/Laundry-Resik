6be746e925ab40bc67f231cb5f7b7a10cb409121
Dimas Atmodjo <dimasputra162003@gmail.com>
2024-06-04 14:39:57 +0700

    landing header fix

--- frontend/src/components/user/HeaderNav.js
+++ frontend/src/components/user/HeaderNav.js
@@ -10,27 +10,27 @@ const HeaderNav = () => {
             <div className="row">
             <div className="col-12 bg-white">
                 <nav className="d-flex justify-content-between align-items-center">
-                <img src={logo} alt="" height={58} />
-                <div className="d-flex justify-content-around align-items-center">
-                    <NavLink to="/" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
-                        Beranda
-                    </NavLink>
-                    <NavLink to="/layanan" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
-                        Layanan
-                    </NavLink>
-                    <NavLink to="/pemesanan" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
-                        Pemesanan
-                    </NavLink>
-                    <NavLink to="/testimoni" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
-                        Testimoni
-                    </NavLink>
-                    <NavLink to="/lacak" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
-                        Cek Pesanan
-                    </NavLink>
-                </div>
-                <div className="d-flex align-items-center">
-                    <a href="/login" style={{visibility:"hidden"}} className="py-3 px-5 btn btn-secondary fw-bold">Masuk</a>
-                </div>
+                    <img src={logo} alt="" height={58} />
+                    <div className="d-flex justify-content-around align-items-center">
+                        <NavLink to="/" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
+                            Beranda
+                        </NavLink>
+                        <NavLink to="/layanan" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
+                            Layanan
+                        </NavLink>
+                        <NavLink to="/pemesanan" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
+                            Pemesanan
+                        </NavLink>
+                        <NavLink to="/testimoni" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
+                            Testimoni
+                        </NavLink>
+                        <NavLink to="/lacak" className={({ isActive }) => isActive ? 'nav-item nav-link active' : 'nav-item nav-link'}>
+                            Cek Pesanan
+                        </NavLink>
+                    </div>
+                    <div className="d-none align-items-center">
+                        <a href="/login" style={{visibility:"hidden"}} className="py-3 px-5 btn btn-secondary fw-bold">Masuk</a>
+                    </div>
                 </nav>
             </div>
             </div>
